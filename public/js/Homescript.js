53131c16efc215f9d6b4607c5b717eff4382c328
Oreniscool <coelhomelini@gmail.com>
2024-02-25 11:55:05 +0530

    Fixed login on about us page and removed some extra stuff left out of dev

--- public/js/Homescript.js
+++ public/js/Homescript.js
@@ -1,22 +1,21 @@
-const animatable = document.querySelectorAll('.animatable')
-console.log(animatable)
+const animatable = document.querySelectorAll('.animatable');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       if (entry.target.classList.contains('animateLeftDisabled')) {
-        entry.target.classList.add('animateLeft', 'show')
+        entry.target.classList.add('animateLeft', 'show');
       } else {
-        entry.target.classList.add('animateRight', 'show')
+        entry.target.classList.add('animateRight', 'show');
       }
     } else {
       if (entry.target.classList.contains('animateLeftDisabled')) {
-        entry.target.classList.remove('animateLeft', 'show')
+        entry.target.classList.remove('animateLeft', 'show');
       } else {
-        entry.target.classList.remove('animateRight', 'show')
+        entry.target.classList.remove('animateRight', 'show');
       }
     }
-  })
-})
+  });
+});
 animatable.forEach((animatables) => {
-  observer.observe(animatables)
-})
+  observer.observe(animatables);
+});
